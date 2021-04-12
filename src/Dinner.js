a49ba411f1862413ac91b5047337999a69bb3b89
kashifnawaz <kashi030@hotmail.com>
2021-04-12 23:32:07 +0500

    created Dec.css

--- src/Dinner.js
+++ src/Dinner.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import Dinner from './dinner.css';
+import Dec from 'Dec.css';
+
 
 function Dinner (props){
-    return <div><h1>I am working {props.myName}</h1>
+    return <div className = "myName"><h1>I am working {props.myName}</h1>
     <div><h2>Successful this time {props.feeling}</h2>
     <div><h3>Very happy now {props.why}</h3></div>
     </div>
