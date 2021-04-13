06164e777b6cad9349e3a0575dae3fc33f45b494
kashifnawaz <kashi030@hotmail.com>
2021-04-13 17:49:00 +0500

    trying

--- src/Dinner.js
+++ src/Dinner.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import Dec from './Dec.css';
+//import React from 'react';
+//import Dec from './Dec.css';
 
 
 
-function Dinner (props){
+/*function Dinner (props){
     return <div className = "myName"><h1>I am working {props.myName}</h1>
     <div className = "yourName"><h2>Successful this time {props.feeling}</h2>
     <div className = "hisName"><h3>Very happy now {props.why}</h3></div>
@@ -12,4 +12,4 @@ function Dinner (props){
        
 }      
 
-export default Dinner;
+export default Dinner;*/
